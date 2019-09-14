226dfd2d24deb732d6d7d867594922d67a4da607
wupeng <wupengforIT@163.com>
2019-09-15 00:05:40 +0800

    同步代码

--- public/DemandRecords/src/router/subComponent/task/taskItem.vue
+++ public/DemandRecords/src/router/subComponent/task/taskItem.vue
@@ -6,14 +6,14 @@
         <div class="content">
             <el-card class="card-item" v-for="item in mineChargeList">
                 <div class="inner-card clearfix">
-                    <span class="task-state"><i class="el-icon-time"></i>{{item.taskProDesc}}</span>
+                    <span :class="item.bgc"><i :class="item.icon"></i>{{item.taskProDesc}}</span>
                     <span class="charge_per">{{item.nickName}}</span>
                 </div>
                 <div class="task-title">{{item.taskTitle}}</div>
                 <div class="trips clearfix">
                     <span class="task-id trip" v-if="item.taskId">任务编号：{{item.taskId}}</span>
                     <span class="task-id trip" v-if="item.belong">所属项目：{{item.belongDesc}}</span>
-                    <span :class="['task-id',currentTime < item.endDate ? 'trip':'trip-out-time']" v-if="item.endDate">截止时间：{{item.endDate | timeFormat}}</span>
+                    <span :class="['task-id',currentTime < item.endDate ? 'trip':'trip-out-time']" v-if="item.endDate">截止时间：{{item.endDate}}</span>
                     <i class="el-icon-document-checked font_focus_color icon"></i>
                 </div>
             </el-card>
@@ -88,6 +88,34 @@
                     margin-right: 5px;
                     font-weight: bolder;
                     color: rgb(255, 164, 21);
+                }
+            }
+            span.task-state-ws {
+                font-size: 12px;
+                display: inline-block;
+                background-color: rgba(250, 90, 85, 0.1);
+                padding: 5px 10px;
+                height: 24px;
+                line-height: 14px;
+                float: left;
+                i {
+                    margin-right: 5px;
+                    font-weight: bolder;
+                    color: rgb(250, 90, 85);
+                }
+            }
+            span.task-state-wc {
+                font-size: 12px;
+                display: inline-block;
+                background-color: rgba(34, 215, 187, 0.1);
+                padding: 5px 10px;
+                height: 24px;
+                line-height: 14px;
+                float: left;
+                i {
+                    margin-right: 5px;
+                    font-weight: bolder;
+                    color: rgb(34, 215, 187);
                 }
             }
             span.charge_per {
@@ -269,8 +297,20 @@
                 this.mineChargeList = data.returnData;
                 this.mineChargeList.map(item => {
                     item.endDate = moment(item.endDate).format('YYYY-MM-DD');
-                })
+                });
                 console.log(this.mineChargeList)
+                this.mineChargeList.forEach(item => {
+                    if (item.taskPro == 0) {
+                        item.icon = 'el-icon-time';
+                        item.bgc = 'task-state-ws';
+                    } else if (item.taskPro == 1) {
+                        item.icon = 'el-icon-remove-outline';
+                        item.bgc = 'task-state';
+                    } else if (item.taskPro == 2) {
+                        item.icon = 'el-icon-circle-check';
+                        item.bgc = 'task-state-wc';
+                    }
+                })
             }, error => {
             });
         },

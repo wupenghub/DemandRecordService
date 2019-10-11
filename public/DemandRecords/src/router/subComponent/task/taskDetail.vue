7c846fef227839cdd024d89c43731459086cf091
wupengforIT <wupengforIT@163.com>
2019-10-11 10:49:19 +0800

    同步代码

--- public/DemandRecords/src/router/subComponent/task/taskDetail.vue
+++ public/DemandRecords/src/router/subComponent/task/taskDetail.vue
@@ -84,12 +84,12 @@
                     </div>
                 </div>
                 <div class="detail-nav">
-                    <router-link :to="tab.path" :class="['tab',tab.select?'select':'',tab.selectTabStyle]" v-for="tab in tabs">
+                    <a @click="chooseTab(tab)" :data-select-tab="tab.selectTabStyle" :class="['tab',tab.selectTabStyle]" v-for="tab in tabs">
                         <span :class="[tab.icon,'icon']"></span>
                         <span class="descr">{{tab.descr}}
                             <i v-show="tab.id != tabs.length" class="divider-line"></i>
                         </span>
-                    </router-link>
+                    </a>
                 </div>
                 <div :class="['comment-body',!showCommentText?'show-animation':'hide-animation']">
                     <span :class="['charge-man']">{{this.$store.state.chooseTask && this.$store.state.chooseTask.nickName}}</span>
@@ -144,41 +144,43 @@
                 endTime: new Date(),
                 updateTime: false,
                 queryTastProList: true,
+                currentTab: null,
                 tabs: [
                     {
                         id: 1,
                         menuCode: 'taskInfo',
                         descr: '任务信息',
                         icon: 'fa fa-tasks',
-                        path: '/taskDetail/taskInfo'
+                        selectTabStyle:''
                     },
                     {
                         id: 2,
                         menuCode: 'sonTask',
                         descr: '子任务',
                         icon: 'el-icon-s-operation',
-                        path: '/taskDetail/sonTask'
+                        selectTabStyle:''
                     },
                     {
                         id: 3,
                         menuCode: 'connectTask',
                         descr: '关联任务',
                         icon: 'fa fa-link',
-                        path: '/taskDetail/connectTask'
+                        path: '/taskDetail/connectTask',
+                        selectTabStyle:''
                     },
                     {
                         id: 4,
                         menuCode: 'workTime',
                         descr: '任务工时',
                         icon: 'el-icon-time',
-                        path: '/taskDetail/workTime'
+                        selectTabStyle:''
                     },
                     {
                         id: 5,
                         menuCode: 'file',
                         descr: '任务附件',
                         icon: 'el-icon-paperclip',
-                        path: '/taskDetail/file'
+                        selectTabStyle:''
                     }
                 ],
                 taskStateList: []
@@ -276,14 +278,17 @@
                     }
                 }
             },
-            $route(to, from) {
-                this.tabs.forEach(item=>{
-                    if(item.path == to.path){
+            'currentTab': function (newval) {
+                this.tabs.forEach(item => {
+                    if (item == newval) {
                         item.selectTabStyle = 'select-tab';
-                    }else {
+                    } else {
                         item.selectTabStyle = '';
                     }
                 });
+                this.tabs.forEach(item => {
+                    console.log(JSON.stringify(item, null, '  '));
+                })
             }
         },
         methods: {
@@ -313,7 +318,9 @@
             },
             chooseEndTime() {
                 this.updateTime = true;
-
+            },
+            chooseTab(tab) {
+                this.currentTab = tab;
             }
         },
         components: {
@@ -368,14 +375,14 @@
 
     /deep/ .el-dialog__body {
         padding: 25px 40px !important;
-
     }
 
     /deep/ .el-dialog {
         width: 980px !important;
         position: relative;
         border-radius: 5px !important;
-        overflow: auto !important;
+        overflow-y: auto !important;
+        overflow-x: hidden;
         height: 580px;
         margin-top: 50px !important;
     }
@@ -534,11 +541,6 @@
                 border-bottom: 2px solid #22d7bb;
 
             }
-            .select {
-                border-bottom: 2px solid #22d7bb;
-                color: #22d7bb;
-
-            }
         }
     }
 
@@ -664,7 +666,8 @@
             }
         }
     }
-    .select-tab{
+
+    .select-tab {
         color: #22d7bb !important;
         border-bottom: 2px solid #22d7bb;
     }

738c1c90a1a4f08bdc5b53b011efd571df88688c
wupeng <wupengforIT@163.com>
2019-10-13 23:23:23 +0800

    同步代码

--- public/DemandRecords/src/router/taskDetail/taskInfo.vue
+++ public/DemandRecords/src/router/taskDetail/taskInfo.vue
@@ -20,16 +20,22 @@
                 <div class="task-info-item project-label">
                     <span class="desc">标签：</span>
                     <div class="label-info item-info">
-                        <span class="label-shape"  v-for="item in taskLabels" :key="item.labelCode" :style="{background:item.bgColor,color:item.fontColor}">
+                        <span class="label-shape" v-for="item in taskLabels" :key="item.labelCode"
+                              :style="{background:item.bgColor,color:item.fontColor}">
                             {{item.icon}}
                         </span>
+                        <span class="add-label"><i class="fa fa-plus label-icon"></i>添加标签</span>
                     </div>
                 </div>
                 <div class="task-info-item project-part-in">
                     <span class="desc">参与人：</span>
                     <div class="part-in-info item-info">
-                        <span class="fa fa-user icon"></span>
-                        <span>{{taskInfo && taskInfo.projectName}}</span>
+                        <span class="part-in-per" v-for="item in partInPers" :key="item.partInPerCode">
+                            {{item.partInPerName}}
+                        </span>
+                        <span class="add-per">
+                            <i class="fa fa-plus"></i>
+                        </span>
                     </div>
                 </div>
             </div>
@@ -51,7 +57,8 @@
                 requestData: true,
                 taskInfo: null,
                 taskInfoItems: [],
-                taskLabels: []
+                taskLabels: [],
+                partInPers: []
             }
         },
         created() {
@@ -71,6 +78,7 @@
                     this.renderPage(data);
                     console.log(JSON.stringify(data, null, '  '));
                     this.taskLabels = data.resultData.taskLabelList;
+                    this.partInPers = data.resultData.partPerList;
                 }, error => {
                     this.requestData = false;
                 }, true);
@@ -90,6 +98,18 @@
         .load {
             margin-top: 10px;
         }
+        .priority-info {
+            height: 32px;
+            width: 200px;
+            padding: 5px 15px;
+            margin-top: -5px;
+        }
+        .priority-info:hover {
+            height: 32px;
+            border: 1px solid #22d7bb;
+            border-radius: 3px;
+            padding: 4px 14px;
+        }
         .task-info-box {
             .task-info-item {
                 width: 33.333%;
@@ -102,7 +122,7 @@
             }
             .desc {
                 color: #888 !important;
-                font-size: 16px;
+                font-size: 14px;
                 line-height: 20px;
             }
             .project-name {
@@ -122,7 +142,44 @@
                     display: inline-block;
                     margin: 0 8px 4px 0;
                 }
-                .label-shape:hover{
+                .label-shape:hover {
+                    cursor: pointer;
+                }
+                .add-label {
+                    display: inline-block;
+                    height: 22px;
+                    line-height: 12px;
+                    padding: 5px 10px;
+                    color: #aaa;
+                    background-color: #eee;
+                    border-radius: 3px;
+                    font-size: 14px;
+                    cursor: pointer;
+                }
+            }
+            .project-part-in {
+                .part-in-per {
+                    display: inline-block;
+                    width: 30px;
+                    height: 30px;
+                    border-radius: 15px;
+                    background-color: rgb(45, 188, 255);
+                    color: white;
+                    line-height: 30px;
+                    font-size: 12px;
+                    text-align: center;
+                    float: left;
+                    margin-right: 5px;
+                }
+                .add-per {
+                    display: inline-block;
+                    width: 30px;
+                    height: 30px;
+                    border-radius: 15px;
+                    border: 1px dashed #aaa;
+                    float: left;
+                    line-height: 30px;
+                    text-align: center;
                     cursor: pointer;
                 }
             }
@@ -132,6 +189,10 @@
     .icon {
         color: #22d7bb;
         font-size: 18px;
+        margin-right: 5px;
+    }
+
+    .label-icon {
         margin-right: 5px;
     }
 

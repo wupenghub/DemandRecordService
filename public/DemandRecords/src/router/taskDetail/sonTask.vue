7b5bd7339571687c0756bb9c41359d1691f78fdb
wupengforIT <wupengforIT@163.com>
2019-10-23 17:26:20 +0800

    同步代码

--- public/DemandRecords/src/router/taskDetail/sonTask.vue
+++ public/DemandRecords/src/router/taskDetail/sonTask.vue
@@ -2,7 +2,20 @@
     <div class="son-task">
         <loading v-show="requestData" class="load"></loading>
         <div class="son-task-info">
-            <div class="task-list" v-if="!this.sonTaskList || this.sonTaskList.length == 0">
+            <div class="son-task-list">
+                <div class="son-task-item" v-for="taskItem in sonTaskList">
+                    <span class="pro-state" :style="{backgroundColor:taskItem.bgColor}">
+                        <i :class="taskItem.icon" :style="{color:taskItem.fontColor}"></i>
+                        {{taskItem.proDesc}}
+                    </span>
+                    <i class="fa fa-navicon (alias) left-nav"></i>
+                    <div class="task-item-right clearfix">
+                        <span style="display: inline-block">吴鹏</span>
+                        <span class="charge-man">
+                            吴鹏
+                        </span>
+                    </div>
+                </div>
                 <span><i class="el-icon-plus add-son-task-icon"></i>新建子任务</span>
             </div>
             <comments @replyComment="replyComment"
@@ -16,6 +29,7 @@
     import loading from '../common/loading.vue';
     import utils from '../../utils/utils.js';
     import comments from '../comments/comments.vue';
+
     export default {
         data() {
             return {
@@ -40,9 +54,9 @@
         },
         props: ['taskId'],
         components: {
-            loading,comments
+            loading, comments
         },
-        methods:{
+        methods: {
             replyComment(commentId) {
                 this.$emit('sendComment', commentId);
             },
@@ -57,15 +71,59 @@
         .load {
             margin-top: 10px;
         }
+
         .son-task-info {
-            .task-list {
+            .son-task-list {
                 padding-left: 30px;
-                height: 46px;
                 margin-bottom: 26px;
-                line-height: 46px;
-                color: #22d7bb;
                 font-size: 14px;
                 cursor: pointer;
+                .son-task-item {
+                    padding: 0 20px;
+                    border-bottom: 1px solid rgba(0, 0, 0, .1);
+                    position: relative;
+                    height: 50px;
+                    line-height: 50px;
+                    .pro-state {
+                        font-size: 12px;
+                        display: inline-block;
+                        padding: 5px 10px;
+                        height: 24px;
+                        line-height: 14px;
+                        border-radius: 2px;
+                    }
+                    .left-nav {
+                        color: #aaa;
+                        position: absolute;
+                        left: 5px;
+                        top: 19px;
+                        display: none;
+                    }
+                    .task-item-right {
+                        float: right;
+                        height: 50px;
+                        /*line-height: 50px;*/
+                        .charge-man {
+                            display: inline-block;
+                            width: 24px;
+                            height: 24px;
+                            line-height: 24px;
+                            text-align: center;
+                            overflow: hidden;
+                            font-size: 12px;
+                            color: white;
+                            background: rgb(45, 188, 255);
+                            border-radius: 12px;
+                        }
+                    }
+                }
+                .son-task-item:hover {
+                    box-shadow: 0 1px 8px 5px rgba(0, 0, 0, .1);
+                    border-bottom: none;
+                }
+                .son-task-item:hover .left-nav {
+                    display: inline-block;
+                }
             }
             .add-son-task-icon {
                 margin-right: 10px;
